e221fd10db0c7cd642cf54bc42944aa7cc73faa7
Lakshman <lakshman14334@gmail.com>
2022-10-26 21:59:50 -0400

    update

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -12,13 +12,13 @@ def read_yaml(path_to_yaml:str) -> dict:
 def create_directory(dirs:list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
-        logging.info(f"directory is created at {dir_path}")
+        logging.info(f"directory is created at: {dir_path}")
 
 def save_local_df(data, data_path, index_status=False):
     data.to_csv(data_path, index=index_status)
-    logging.info(f'data is saved at {data_path}')
+    logging.info(f'data is saved at: {data_path}')
 
 def save_reports(report: dict, report_path: str):
     with open(report_path, 'w') as f:
         json.dump(report, f, indent=4)
-    logging.info(f'reports are saved at {report_path}')
+    logging.info(f'reports are saved at: {report_path}')
