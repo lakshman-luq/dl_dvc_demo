0e235d3717f1cfa849df7f927c5be28c920583bd
Lakshman <lakshman14334@gmail.com>
2022-10-26 21:54:06 -0400

    stage 2 created and base model is ready

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,10 +1,11 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse, os
 import pandas as pd 
 import shutil
 from tqdm import tqdm
 import logging
+import io
 
 logging_str = '[%(asctime)s: %(levelname)s: %(module)s]: %(message)s'
 log_dir = 'logs'
@@ -29,7 +30,31 @@ def prepare_base_model(config_path, params_path):
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
-    model = get_VGG_16_model(input_shape=params['IMAGE_SIZE'])
+    model = get_VGG_16_model(input_shape=params['IMAGE_SIZE'], model_path=base_model_path)
+
+    full_model = prepare_model(
+        model, 
+        CLASSES = params['CLASSES'],
+        freeze_all = True,
+        freeze_till = None,
+        learning_rate = params['LEARNING_RATE']
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts['UPDATED_BASE_MODEL_NAME']
+    )
+
+    def log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn=lambda x: stream.write(f'{x}\n'))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f'full model summary: \n{log_model_summary(full_model)}')
+
+    full_model.save(update_base_model_path)
+
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
